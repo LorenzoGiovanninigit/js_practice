4b6c50a48574112bf1f0587f69e66303c7fafe8d
Lorenzo Giovannini <lorenzogiovannini19@gmail.com>
2021-03-18 14:42:29 +0100

    adding sumAll, no issues

---
+++ sumAll/sumAll.js
@@ -0,0 +1,23 @@
+const sumAll = function(a,b) {
+    let sum = 0;
+  if(a < 0 || b < 0){
+      return 'ERROR'
+  }
+  else if(typeof(a) != 'number' || typeof(b) != 'number'){
+      return "ERROR"
+  }
+  else if(a < b ){
+      for(let i = a; i <= b; i++){
+          sum = sum + i
+
+      }
+  } else {
+      for(let i = b; i<=a; i++){
+          sum = sum  + i
+      }
+  }
+  return sum
+  
+}
+
+module.exports = sumAll
